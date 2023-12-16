4f61732a1c1f818748b624d2c318dd967e5d5abf
Charan Teja Reddy <charante@codeaurora.org>
2023-12-17 00:20:21 +0700

    kernel: trigger lmkd specific triggers during emergency
    Ensure that only LMKD registered triggers gets an event during the
    manual generation of emergency trigger.
    
    Change-Id: Ibb0bf447ae225ddacdad809ba3418ed33c47980a
    Signed-off-by: Charan Teja Reddy <charante@codeaurora.org>
    Signed-off-by: shygosh <shygosh@proton.me>

--- include/linux/psi_types.h
+++ include/linux/psi_types.h
@@ -120,6 +120,9 @@ struct psi_trigger {
 	 * events to one per window
 	 */
 	u64 last_event_time;
+
+	/* Task that created the trigger */
+	char comm[TASK_COMM_LEN];
 };
 
 struct psi_group {
--- kernel/sched/psi.c
+++ kernel/sched/psi.c
@@ -568,6 +568,8 @@ void psi_emergency_trigger(void)
 		return;
 
 	list_for_each_entry(t, &group->triggers, node) {
+		if (strcmp(t->comm, "lmkd"))
+			continue;
 		/* Generate an event */
 		if (cmpxchg(&t->event, 0, 1) == 0)
 			wake_up_interruptible(&t->event_wait);
@@ -1074,6 +1076,7 @@ struct psi_trigger *psi_trigger_create(struct psi_group *group,
 	t->event = 0;
 	t->last_event_time = 0;
 	init_waitqueue_head(&t->event_wait);
+	get_task_comm(t->comm, current);
 
 	mutex_lock(&group->trigger_lock);
 
