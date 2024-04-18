c9f58f460ee620b5476d2c26ad515050d4c404df
shygosh <mossynine@gmail.com>
2024-04-19 01:29:44 +0700

    init: Improve retrofit dynamic partition support
    Add super_partition handling for retrofit dynamic partition support.
    This retains the ability to boot on stock partition layout.
    
    Signed-off-by: shygosh <mossynine@gmail.com>

--- init/do_mounts.c
+++ init/do_mounts.c
@@ -300,6 +300,8 @@ EXPORT_SYMBOL_GPL(name_to_dev_t);
 
 static int __init root_dev_setup(char *line)
 {
+	if (super_partition)
+		return 0;
 	strlcpy(saved_root_name, line, sizeof(saved_root_name));
 	return 1;
 }
@@ -324,7 +326,7 @@ __setup("early_userspace=", early_userspace_setup);
 #endif
 static int __init rootwait_setup(char *str)
 {
-	if (*str)
+	if (*str || super_partition)
 		return 0;
 	root_wait = 1;
 	return 1;
