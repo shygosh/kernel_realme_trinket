59cd547830343ced917004daa1f7d9cf196ade36
shygosh <shygosh@proton.me>
2024-09-16 15:03:19 +0700

    sched/fair: Account for all running tasks for load balance
    torvalds/linux@5e23e4744315
    
    Signed-off-by: shygosh <shygosh@proton.me>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9343,6 +9343,7 @@ struct sg_lb_stats {
 	unsigned long group_capacity;
 	unsigned long group_util; /* Total utilization of the group */
 	unsigned int sum_nr_running; /* Nr tasks running in the group */
+	unsigned int sum_nr_running_all; /* All Nr tasks running in the group */
 	unsigned int idle_cpus;
 	unsigned int group_weight;
 	enum group_type group_type;
@@ -9390,7 +9391,12 @@ static inline void init_sd_lb_stats(struct sd_lb_stats *sds)
 		.busiest_stat = {
 			.avg_load = 0UL,
 			.sum_nr_running = 0,
+			.sum_nr_running_all = 0,
 			.group_type = group_other,
+		},
+		.local_stat = {
+			.sum_nr_running = 0,
+			.sum_nr_running_all = 0,
 		},
 	};
 }
@@ -9573,7 +9579,7 @@ static inline int sg_imbalanced(struct sched_group *group)
 static inline bool
 group_has_capacity(struct lb_env *env, struct sg_lb_stats *sgs)
 {
-	if (sgs->sum_nr_running < sgs->group_weight)
+	if (sgs->sum_nr_running_all < sgs->group_weight)
 		return true;
 
 	if ((sgs->group_capacity * 100) >
@@ -9594,7 +9600,7 @@ group_has_capacity(struct lb_env *env, struct sg_lb_stats *sgs)
 static inline bool
 group_is_overloaded(struct lb_env *env, struct sg_lb_stats *sgs)
 {
-	if (sgs->sum_nr_running <= sgs->group_weight)
+	if (sgs->sum_nr_running_all <= sgs->group_weight)
 		return false;
 
 #ifdef CONFIG_SCHED_WALT
@@ -9689,6 +9695,8 @@ static inline void update_sg_lb_stats(struct lb_env *env,
 		sgs->group_load += cpu_runnable_load(rq);
 		sgs->group_util += cpu_util(i);
 		sgs->sum_nr_running += rq->cfs.h_nr_running;
+		sgs->sum_nr_running_all += rq->cfs.h_nr_running +
+			 rq->rt.rt_nr_running + rq->dl.dl_nr_running;
 
 		nr_running = rq->nr_running;
 		if (nr_running > 1)
@@ -9792,7 +9800,7 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 	 * capable CPUs may harm throughput. Maximize throughput,
 	 * power/energy consequences are not considered.
 	 */
-	if (sgs->sum_nr_running <= sgs->group_weight &&
+	if (sgs->sum_nr_running_all <= sgs->group_weight &&
 	    group_smaller_min_cpu_capacity(sds->local, sg))
 		return false;
 
@@ -9952,7 +9960,7 @@ static inline void update_sd_lb_stats(struct lb_env *env, struct sd_lb_stats *sd
 		 */
 		if (prefer_sibling && sds->local &&
 		    group_has_capacity(env, local) &&
-		    (sgs->sum_nr_running > local->sum_nr_running + 1)) {
+		    (sgs->sum_nr_running_all > local->sum_nr_running_all + 1)) {
 			sgs->group_no_capacity = 1;
 			sgs->group_type = group_classify(sg, sgs);
 		}
@@ -10158,8 +10166,8 @@ void fix_small_imbalance(struct lb_env *env, struct sd_lb_stats *sds)
 	 */
 	if (env->sd->flags & SD_ASYM_CPUCAPACITY &&
 		busiest->group_type == group_overloaded &&
-		busiest->sum_nr_running > busiest->group_weight &&
-		local->sum_nr_running < local->group_weight &&
+		busiest->sum_nr_running_all > busiest->group_weight &&
+		local->sum_nr_running_all < local->group_weight &&
 		local->group_capacity < busiest->group_capacity)
 		env->imbalance = busiest->load_per_task;
 }
@@ -10264,7 +10272,7 @@ static inline void calculate_imbalance(struct lb_env *env, struct sd_lb_stats *s
 	 */
 	if (busiest->group_type == group_misfit_task &&
 	    (env->idle == CPU_NEWLY_IDLE ||
-	     local->sum_nr_running < local->group_weight)) {
+	     local->sum_nr_running_all < local->group_weight)) {
 		env->imbalance = max_t(long, env->imbalance,
 				       busiest->group_misfit_task_load);
 	}
