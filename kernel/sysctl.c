af3ae8712efd563cf33ba8bbe640d5d1c884b1fe
shygosh <shygosh@proton.me>
2024-10-08 11:04:03 +0700

    sysctl: Fix undefined symbol
    ld.lld: error: undefined symbol: sched_updown_migrate_handler
    >>> referenced by sysctl.c
    >>>               kernel/sysctl.o:(kern_table) in archive built-in.o
    >>> referenced by sysctl.c
    >>>               kernel/sysctl.o:(kern_table) in archive built-in.o
    
    Signed-off-by: shygosh <shygosh@proton.me>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -480,6 +480,7 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= proc_dointvec,
 	},
 #endif
+#ifdef CONFIG_CGROUP_SCHED
 	{
 		.procname	= "sched_upmigrate_alt",
 		.data		= &sysctl_sched_capacity_margin_up,
@@ -494,6 +495,7 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= sched_updown_migrate_handler,
 	},
+#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_min_granularity_ns_alt",
