08aab02120f227dd5418e6d92291cbc3c284ef86
shygosh <shygosh@proton.me>
2024-12-29 08:17:50 +0700

    schedutil: Initialize rate_limit according to CONFIG_HZ
    Signed-off-by: shygosh <shygosh@proton.me>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -579,10 +579,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us =
-				cpufreq_policy_transition_delay_us(policy);
-	tunables->down_rate_limit_us =
-				cpufreq_policy_transition_delay_us(policy);
+	tunables->up_rate_limit_us = max((1000 / CONFIG_HZ), 1) * USEC_PER_MSEC;
+	tunables->down_rate_limit_us = max((1000 / CONFIG_HZ), 1) * USEC_PER_MSEC;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
