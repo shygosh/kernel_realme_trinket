b693b294339f3b2f56fe8e2505a9555e63d98a50
shygosh <shygosh@proton.me>
2025-01-02 16:50:39 +0700

    mm: Increase max compact and reclaim retries
    Perhaps increasing max number of retries can prove to be beneficial
    for memory-constrained system. So let's try it.
    
    Signed-off-by: shygosh <shygosh@proton.me>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3581,7 +3581,7 @@ out:
  * Maximum number of compaction retries wit a progress before OOM
  * killer is consider as the only way to move forward.
  */
-#define MAX_COMPACT_RETRIES 16
+#define MAX_COMPACT_RETRIES 32
 
 #ifdef CONFIG_COMPACTION
 /* Try memory compaction for high-order allocations before reclaim */
