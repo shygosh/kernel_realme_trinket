1619decbf0d9c8597053586380630714a9726dbc
shygosh <shygosh@proton.me>
2025-01-02 16:50:39 +0700

    mm: Increase max compact and reclaim retries
    Perhaps increasing max number of retries can prove to be beneficial
    for memory-constrained system. So let's try it.
    
    Signed-off-by: shygosh <shygosh@proton.me>

--- mm/internal.h
+++ mm/internal.h
@@ -107,7 +107,7 @@ extern unsigned long highest_memmap_pfn;
  * Maximum number of reclaim retries without progress before the OOM
  * killer is consider the only way forward.
  */
-#define MAX_RECLAIM_RETRIES 16
+#define MAX_RECLAIM_RETRIES 32
 
 /*
  * in mm/vmscan.c:
